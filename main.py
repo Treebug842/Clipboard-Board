521d17ebe4a2a2045712b83fccc25e07111b3c7b
Treebug842 <51908187+Treebug842@users.noreply.github.com>
2021-10-26 01:26:12 +1100

    Update main.py

--- main.py
+++ main.py
@@ -9,50 +9,90 @@ root = tk.Tk()
 buttonTitles = []
 buttonData = []
 
+with open("data", 'rb') as file:
+	buttonTitles, buttonData = pickle.load(file)
+
+# Clear Variables
+buttonTitles = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
+buttonData = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
+
+def save_data():
+	with open("data", 'wb') as file:
+		pickle.dump([buttonTitles, buttonData], file)
+
 root.title("Clipboard Board")
-root.geometry("600x300")
+root.geometry("570x300")
 # root.iconbitmap("lib/icon.ico")
 
 frame1 = tk.Frame(root)
 frame2 = tk.Frame(root)
 frame3 = tk.Frame(root)
 frame4 = tk.Frame(root)
+bFrame = tk.Frame(root)
 
 def changeFrame(framenumber):
 	global currentFrame
 	exec(f"frame{currentFrame}.grid_forget()")
 	exec(f"frame{framenumber}.grid(row=2, column=0, columnspan=11)")
+	exec(f"frame{currentFrame}Button.configure(bg='SystemButtonFace')")
+	exec(f"frame{framenumber}Button.configure(bg='red')")
 	currentFrame = framenumber
 
-frame1Button = tk.Button(root, text="1", font='Helvetica 10 bold', command=lambda: changeFrame(1)); frame1Button.grid(row=0, column=8)
-frame2Button = tk.Button(root, text="2", font='Helvetica 10 bold', command=lambda: changeFrame(2)); frame2Button.grid(row=0, column=9)
-frame3Button = tk.Button(root, text="3", font='Helvetica 10 bold', command=lambda: changeFrame(3)); frame3Button.grid(row=0, column=10)
-frame4Button = tk.Button(root, text="4", font='Helvetica 10 bold', command=lambda: changeFrame(4)); frame4Button.grid(row=0, column=11)
+
+frame1Button = tk.Button(bFrame, text="1", font='Helvetica 12 bold', height=1, width=2, command=lambda:changeFrame(1)); frame1Button.grid(row=0, column=0)
+frame2Button = tk.Button(bFrame, text="2", font='Helvetica 12 bold', height=1, width=2, command=lambda:changeFrame(2)); frame2Button.grid(row=0, column=1)
+frame3Button = tk.Button(bFrame, text="3", font='Helvetica 12 bold', height=1, width=2, command=lambda:changeFrame(3)); frame3Button.grid(row=0, column=2)
+frame4Button = tk.Button(bFrame, text="4", font='Helvetica 12 bold', height=1, width=2, command=lambda:changeFrame(4)); frame4Button.grid(row=0, column=3)
 
 def editClick():
 	global editMode
-	editMode = 1 if editMode == 0 else 0
+	if editMode == 1:
+		editMode = 0
+		editButton.configure(bg="SystemButtonFace")
+	elif editMode == 0:
+		editMode = 1
+		editButton.configure(bg="cyan")
 
-editButton = tk.Button(root, text="⚙️", font='Helvetica 14 bold', command=editClick); editButton.grid(row=0, column=0)
+editButton = tk.Button(root, text="⚙️", font='Helvetica 14 bold', command=editClick); editButton.grid(row=0, column=0, sticky=tk.W)
 title = tk.Label(root, text="Clipboard Board", font='Helvetica 16 bold'); title.grid(row=0, column=6)
+bFrame.grid(row=0, column=8, columnspan=4)
 
 
 class createButtons:
 	def __init__(self, frame, number):
 		self.frame = frame
 		self.number = number
-
-		
-		createCMD = f"self.button = tk.Button({frame}, text='', width=18, height=2, command=lambda: createButtons.click({number}))"
+		createCMD = f"self.button = tk.Button({frame}, text='{buttonTitles[(number - 1)]}', font='Helvetica 14 bold', width=11, height=1, command=lambda: createButtons.click({number}))"
 		exec(createCMD)
 
 
+	@staticmethod
+	def editEntry(buttonNumber):
+		editBox = tk.Tk()
+		editBox.title(f"Edit Button {buttonNumber}")
+		editBox.geometry("300x300")
+
+		titleLabel = tk.Label(editBox, text=f"Edit Title", font='Helvetica 10 bold'); titleLabel.pack(pady=3)
+		titleBox = tk.Entry(editBox, width=20); titleBox.pack(pady=3)
+		dataLabel = tk.Label(editBox, text="Edit Data", font='Helvetica 10 bold'); dataLabel.pack(pady=3)
+		dataBox = tk.Text(editBox, height=10, width=30); dataBox.pack(pady=3)
+
+		editBox.mainloop()
+
+	@staticmethod
+	def copyToClipboard(buttonNumber):
+		print("Copied to clipboard")
 
 
 
 	@staticmethod
 	def click(number):
-		print(f"Button {number} has been clicked")
+		global editMode
+		if editMode == 1:
+			createButtons.editEntry(number)
+		elif editMode == 0:
+			createButtons.copyToClipboard(number)
+
 
 
 
@@ -89,4 +129,5 @@ for i in range (0, 4):
 
 
 frame1.grid(row=2, column=0, columnspan=11)
+frame1Button.configure(bg="red")
 root.mainloop()
