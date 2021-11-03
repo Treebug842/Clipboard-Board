9c28f8c451863ee3ed61959b4f24cb407b03b6dc
Treebug842 <51908187+Treebug842@users.noreply.github.com>
2021-11-03 23:17:32 +1100

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,6 @@ def changeFrame(framenumber):
 	exec(f"frame{framenumber}Button.configure(bg='red')")
 	currentFrame = framenumber
 
-
 frame1Button = tk.Button(bFrame, text="1", font='Helvetica 12 bold', height=1, width=2, command=lambda:changeFrame(1)); frame1Button.grid(row=0, column=0)
 frame2Button = tk.Button(bFrame, text="2", font='Helvetica 12 bold', height=1, width=2, command=lambda:changeFrame(2)); frame2Button.grid(row=0, column=1)
 frame3Button = tk.Button(bFrame, text="3", font='Helvetica 12 bold', height=1, width=2, command=lambda:changeFrame(3)); frame3Button.grid(row=0, column=2)
@@ -60,7 +59,6 @@ editButton = tk.Button(mainwindow, text="⚙️", font='Helvetica 13 bold', comm
 title = tk.Label(mainwindow, text="Clipboard Board", font='Helvetica 16 bold'); title.grid(row=0, column=6)
 bFrame.grid(row=0, column=8, columnspan=4)
 
-
 class createButtons:
 	def __init__(self, frame, number):
 		self.frame = frame
@@ -68,19 +66,17 @@ class createButtons:
 		createCMD = f"self.button = tk.Button({frame}, text='{buttonTitles[(number - 1)]}', font='Helvetica 14 bold', width=11, height=1, command=lambda: createButtons.click({number}), relief='solid', borderwidth=1)"
 		exec(createCMD)
 
-
-
 	@staticmethod
 	def editEntry(buttonNumber):
-		editBox = tk.Tk()
-		editBox.title(f"Edit Button {buttonNumber}")
-		editBox.geometry("300x400")
-
-		titleLabel = tk.Label(editBox, text=f"Edit Title", font='Helvetica 10 bold'); titleLabel.grid(row=0, column=0, columnspan=2)
+		editRoot = tk.Tk()
+		editBox = tk.Frame(editRoot); editBox.pack()
+		editRoot.title(f"Edit Button {buttonNumber}")
+		editRoot.geometry("450x480")
+		titleLabel = tk.Label(editBox, text=f"Edit Title", font='Helvetica 12 bold'); titleLabel.grid(row=0, column=0, columnspan=2, pady=5)
 		titleBox = tk.Entry(editBox, width=20); titleBox.grid(row=1, column=0, columnspan=2)
 		titleBox.insert(0, buttonTitles[buttonNumber - 1])
-		dataLabel = tk.Label(editBox, text="Edit Data", font='Helvetica 10 bold'); dataLabel.grid(row=2, column=0, columnspan=2)
-		dataBox = tk.Text(editBox, height=10, width=40); dataBox.grid(row=3, column=0, columnspan=2, padx=20)
+		dataLabel = tk.Label(editBox, text="Edit Data", font='Helvetica 12 bold'); dataLabel.grid(row=2, column=0, columnspan=2, pady=5)
+		dataBox = tk.Text(editBox, height=20, width=50); dataBox.grid(row=3, column=0, columnspan=2, padx=20)
 		dataBox.insert(tk.INSERT, buttonData[buttonNumber - 1])
 
 		def saveEntry(buttonNumber):
@@ -88,32 +84,24 @@ class createButtons:
 			entry = titleBox.get()
 			buttonTitles[buttonNumber - 1] = entry
 			exec(f"button{buttonNumber}.button.configure(text='{entry}')")
-
 			global buttonData
 			entry = dataBox.get("1.0", "end-1c")
 			buttonData[buttonNumber - 1] = entry
 			save_data()
-			# Dosnt work
-
-			editBox.destroy()
+			editRoot.destroy()
 			editClick()
 
 		def cancelEntry():
-			editBox.destroy()
+			editRoot.destroy()
 			editClick()
 
-
-		saveButton = tk.Button(editBox, text="Save", font='Helvetica 14 bold', command=lambda:saveEntry(buttonNumber), padx=10); saveButton.grid(row=5, column=0, sticky=tk.E)
-		cancelButton = tk.Button(editBox, text="Close", font='Helvetica 14 bold', command=lambda:cancelEntry()); cancelButton.grid(row=5, column=1, sticky=tk.W)
-
+		saveButton = tk.Button(editBox, text="Save", font='Helvetica 14 bold', command=lambda:saveEntry(buttonNumber), padx=10); saveButton.grid(row=5, column=0, sticky=tk.E, pady=5)
+		cancelButton = tk.Button(editBox, text="Close", font='Helvetica 14 bold', command=lambda:cancelEntry()); cancelButton.grid(row=5, column=1, sticky=tk.W, pady=5)
 		editBox.mainloop()
 
 	@staticmethod
 	def copyToClipboard(buttonNumber):
 		pyperclip.copy(buttonData[buttonNumber - 1])
-		print(buttonData[buttonNumber - 1])
-
-
 
 	@staticmethod
 	def click(number):
@@ -123,28 +111,20 @@ class createButtons:
 		elif editMode == 0:
 			createButtons.copyToClipboard(number)
 
-
-
-
-
 for i in range(1, 25):
 	command1 = f"button{i} = createButtons('frame1', {i})"
 	exec(command1)
-
 for i in range(25, 49):
 	command1 = f"button{i} = createButtons('frame2', {i})"
 	exec(command1)
-
 for i in range(49, 73):
 	command1 = f"button{i} = createButtons('frame3', {i})"
 	exec(command1)
-
 for i in range(73, 97):
 	command1 = f"button{i} = createButtons('frame4', {i})"
 	exec(command1)
 
 count = 1
-
 for i in range (0, 4):
 	for x in range(1, 7):
 		for y in range(1, 5):
@@ -152,8 +132,6 @@ for i in range (0, 4):
 			exec(cmd)
 			count += 1
 
-
 frame1.grid(row=2, column=0, columnspan=11)
-
 frame1Button.configure(bg="red")
 mainwindow.mainloop()
